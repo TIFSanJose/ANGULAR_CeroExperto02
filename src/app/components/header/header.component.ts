150288c440f3883cbdf107ccd9438b963dc78810
ramon <ramon@ramon>
2021-09-24 17:45:38 -0300

    v42 Creando componentes manualmente

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-header',
-    template: ` <h1>Hola Header</h1> `
+    selector:    'app-header',
+    templateUrl: './header.component.html'
 })
 export class HeaderComponent {
 
