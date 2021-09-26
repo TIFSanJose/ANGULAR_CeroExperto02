c7336ab41882daaabfa237c465658af8895db152
ramon <ramon@ramon>
2021-09-26 16:40:39 -0300

    v45 Directivas ngIf ngFor. incluye v43 y v44

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
 })
 export class BodyComponent{
 
+    mostrar: boolean = false;
+
+    frase: any = {
+        texto: 'Un gran poder exige una gran responsabilidad',
+        autor: 'Ben Parker'
+    }
+
+    personajes: string[] = ['Spiderman', 'Venom', 'Dr. Octupus', 'Duende Verde'];
 }
